f2d053a7063b62163b8a83d954c13d44ee463013
Darkusagi99 <darkusagi99@gmail.com>
2020-03-22 17:20:52 +0100

    Initialisation plateau de jeu

--- app/src/main/java/com/gmail/darkusagi99/marietetris/GameActivity.kt
+++ app/src/main/java/com/gmail/darkusagi99/marietetris/GameActivity.kt
@@ -1,17 +1,33 @@
 package com.gmail.darkusagi99.marietetris
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_game.*
+import androidx.appcompat.app.AppCompatActivity
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 class GameActivity : AppCompatActivity() {
+
+    var NUM_ROWS = 21
+    var NUM_COLUMNS = 16
+    val BOARD_HEIGHT = 800
+    val BOARD_WIDTH = 720
+
+    lateinit var bitmap: Bitmap
+    lateinit var canvas: Canvas
+    lateinit var paint: Paint
+    lateinit var linearLayout: LinearLayout
+
     private val mHideHandler = Handler()
     private val mHidePart2Runnable = Runnable {
 
@@ -43,7 +59,17 @@ class GameActivity : AppCompatActivity() {
         val scoreField = findViewById<TextView>(R.id.scoreText)
         scoreField.text = "0"
 
+        bitmap = Bitmap.createBitmap(BOARD_WIDTH, BOARD_HEIGHT, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(bitmap)
+        paint = Paint()
+        linearLayout = findViewById<View>(R.id.game_board) as LinearLayout
+
         mVisible = true
+
+
+        // Paint the initial matrix (frontend)
+        DrawScreen()
+
     }
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
@@ -110,4 +136,108 @@ class GameActivity : AppCompatActivity() {
          */
         private val UI_ANIMATION_DELAY = 300
     }
+
+    fun DrawScreen() { // Paint the game board background
+        paint.color = Color.BLACK
+        canvas.drawRect(0f, 0f, BOARD_WIDTH.toFloat(), BOARD_HEIGHT.toFloat(), paint)
+
+        val colWidth = (BOARD_WIDTH / (NUM_COLUMNS)).toFloat()
+        val rowHeight = (BOARD_HEIGHT / (NUM_ROWS)).toFloat()
+
+        paint.color = Color.LTGRAY
+        canvas.drawRect(0f, 0f, colWidth, BOARD_HEIGHT.toFloat(), paint)
+        canvas.drawRect(colWidth*11, 0f, colWidth*12, BOARD_HEIGHT.toFloat(), paint)
+        canvas.drawRect(0f, rowHeight*20, colWidth*12, BOARD_HEIGHT.toFloat(), paint)
+
+        // Paint the grid on the game board
+        paint.color = Color.WHITE
+        for (i in 0..NUM_ROWS) {
+            canvas.drawLine(
+                0f, i * (BOARD_HEIGHT / (NUM_ROWS)).toFloat(), BOARD_WIDTH.toFloat(),
+                i * (BOARD_HEIGHT / (NUM_ROWS)).toFloat(), paint
+            )
+        }
+        for (i in 1..NUM_COLUMNS - 5) {
+            canvas.drawLine(
+                i * colWidth, 0f,
+                i * colWidth, BOARD_HEIGHT.toFloat(), paint
+            )
+        }
+        // Paint the tetris blocks
+        /*for (i in 3 until NUM_ROWS - 3) {
+            for (j in 3 until NUM_COLUMNS - 3) {
+                if (gameMatrix.get(i).get(j).getState() == 1) {
+                    paint.color = gameMatrix.get(i).get(j).getColor()
+                    canvas.drawRect(
+                        (j - 3) * (BOARD_WIDTH / (NUM_COLUMNS - 6)).toFloat(),
+                        (i - 3) * (BOARD_HEIGHT / (NUM_ROWS - 6)).toFloat(),
+                        (j + 1 - 3) * (BOARD_WIDTH / (NUM_COLUMNS - 6)).toFloat(),
+                        (i + 1 - 3) * (BOARD_HEIGHT / (NUM_ROWS - 6)).toFloat(),
+                        paint
+                    )
+                }
+            }
+        }
+        // Paint borders to the tetris blocks
+        for (i in 3 until NUM_ROWS - 3) {
+            for (j in 3 until NUM_COLUMNS - 3) {
+                if (gameMatrix.get(i).get(j).getState() == 1) {
+                    paint.color = Color.BLACK
+                    canvas.drawLine(
+                        (j - 3) * (BOARD_WIDTH / (NUM_COLUMNS - 6)).toFloat(),
+                        (i - 3) * (BOARD_HEIGHT / (NUM_ROWS - 6)).toFloat(),
+                        (j - 3) * (BOARD_WIDTH / (NUM_COLUMNS - 6)).toFloat(),
+                        (i + 1 - 3) * (BOARD_HEIGHT / (NUM_ROWS - 6)).toFloat(),
+                        paint
+                    )
+                    canvas.drawLine(
+                        (j - 3) * (BOARD_WIDTH / (NUM_COLUMNS - 6)).toFloat(),
+                        (i - 3) * (BOARD_HEIGHT / (NUM_ROWS - 6)).toFloat(),
+                        (j + 1 - 3) * (BOARD_WIDTH / (NUM_COLUMNS - 6)).toFloat(),
+                        (i - 3) * (BOARD_HEIGHT / (NUM_ROWS - 6)).toFloat(),
+                        paint
+                    )
+                    canvas.drawLine(
+                        (j + 1 - 3) * (BOARD_WIDTH / (NUM_COLUMNS - 6)).toFloat(),
+                        (i - 3) * (BOARD_HEIGHT / (NUM_ROWS - 6)).toFloat(),
+                        (j + 1 - 3) * (BOARD_WIDTH / (NUM_COLUMNS - 6)).toFloat(),
+                        (i + 1 - 3) * (BOARD_HEIGHT / (NUM_ROWS - 6)).toFloat(),
+                        paint
+                    )
+                    canvas.drawLine(
+                        (j - 3) * (BOARD_WIDTH / (NUM_COLUMNS - 6)).toFloat(),
+                        (i + 1 - 3) * (BOARD_HEIGHT / (NUM_ROWS - 6)).toFloat(),
+                        (j + 1 - 3) * (BOARD_WIDTH / (NUM_COLUMNS - 6)).toFloat(),
+                        (i + 1 - 3) * (BOARD_HEIGHT / (NUM_ROWS - 6)).toFloat(),
+                        paint
+                    )
+                }
+            }
+        }*/
+        /*if (!gameInProgress) {
+            val textView =
+                findViewById<View>(R.id.game_over_textview) as TextView
+            textView.visibility = View.VISIBLE
+            val textView2 =
+                findViewById<View>(R.id.game_over_textview2) as TextView
+            textView2.visibility = View.VISIBLE
+        } else if (gamePaused) {
+            paint.color = Color.WHITE
+            paint.textAlign = Paint.Align.CENTER
+            paint.textSize = 60f
+            canvas.drawText(
+                "GAME PAUSED",
+                (BOARD_WIDTH / 2.0).toFloat(),
+                (BOARD_HEIGHT / 2.0).toFloat(),
+                paint
+            )
+        }*/
+        // Display the current painting
+        linearLayout.setBackgroundDrawable(BitmapDrawable(bitmap))
+        // Update the score textview
+        /*val textView =
+            findViewById<View>(R.id.game_score_textview) as TextView
+        textView.text = "Score: $score"*/
+    }
+
 }
